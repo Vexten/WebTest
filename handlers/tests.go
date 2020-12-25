a253ea6217fadbf44710e2b8e0d41bd80e6c0aff
Pavel Vex <50640372+Vexten@users.noreply.github.com>
2020-12-25 05:31:52 +0300

    [T-1622] NewTest

--- handlers/tests.go
+++ handlers/tests.go
@@ -3,6 +3,27 @@ import (
 	"net/http"
 )
 
+// Обрабатывает создание нового теста
+func NewTest(w http.ResponseWriter, req *http.Request, sessUs *in.SessUs) error {
+	if req.Method == "GET" {
+		http.Redirect(w, req, `/`, http.StatusFound)
+		return nil
+	}
+	if sessUs.UsId == in.GuestUserId {
+		http.Redirect(w, req, `/`, http.StatusFound)
+		return nil
+	}
+	tName := in.TestName(req.FormValue("TestName"))
+	tDesc := in.TestDesc(req.FormValue("TestDesc"))
+	err := db.CreateTest(tName, tDesc, sessUs.UsId)
+	if err != nil {
+		err = errors.New("Ошибка создания теста newTest " + err.Error())
+		return err
+	}
+	http.Redirect(w, req, `/create`, http.StatusFound)
+	return nil
+}
+
 // Обрабатывает страницу редактирования теста
 func editTest(w http.ResponseWriter, req *http.Request, sessUs *in.SessUs, testId in.TestId) error {
 	ok, err := db.CheckAuthorTest(testId, sessUs.UsId)
