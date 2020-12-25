717b119257cff5a93033676dbed8df4e820a559c
Alexandrmrn <hardesthackerslogin@gmail.com>
2020-12-25 12:48:38 +0300

    [T-1599]

--- helpFun/session.go
+++ helpFun/session.go
@@ -10,6 +10,31 @@ import (
 	"net/http"
 )
 
+// Создает сессию в БД и устанавливает куки у пользователя
+func CreateAndSetSess(w http.ResponseWriter, r *http.Request, session *sessions.Session, id in.UsId) (*in.SessUs, error) {
+	var err error
+	if session == nil {
+		session, err = in.Store.Get(r, in.SessionName)
+		if err != nil {
+			return nil, err
+		}
+	}
+	uid, err := genSessId(id)
+	if err != nil {
+		return nil, err
+	}
+	err = db.CreateSessId(uid)
+	if err != nil {
+		return nil, err
+	}
+	session.Values[in.CookRowName] = uid
+	err = sessions.Save(r, w)
+	if err != nil {
+		return nil, err
+	}
+	return uid, nil
+}
+
 // Генерирует сессию без коллизий в БД
 func genSessId(id in.UsId) (*in.SessUs, error) {
 	for true {
