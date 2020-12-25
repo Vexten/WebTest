807961fd8c488095a9d8e04d79e0159aafca2dae
Alexandrmrn <hardesthackerslogin@gmail.com>
2020-12-25 13:24:17 +0300

    T-1609

--- DBWorker/topics.go
+++ DBWorker/topics.go
@@ -1,5 +1,28 @@
 import "context"
 
+// Создает новую тему в тесте
+func CreateTopic(topicName in.TopicName, topicDesc in.TopicDesc, testId in.TestId) error {
+	sql := `insert into topic(name,"desc",test_id) values ($1,$2,$3)`
+	_, err := db.Exec(context.Background(), sql, topicName, topicDesc, testId)
+	return err
+}
+
+// Возвращает true, если пользователь является автором темы
+func CheckAuthorTopic(topicId in.TopicId, usId in.UsId) (bool, error) {
+	sql := `
+select *
+from topic join test_author on topic.test_id = test_author.test_id
+where id = $1 and us_id = $2`
+	res, err := db.Exec(context.Background(), sql, topicId, usId)
+	if err != nil {
+		return false, err
+	}
+	if res[len(res)-1] == '0' {
+		return false, nil
+	}
+	return true, nil
+}
+
 // Получает все темы теста
 func GetTopics(testId in.TestId) ([]in.Topic, error) {
 	sql := `select id, name, "desc"
