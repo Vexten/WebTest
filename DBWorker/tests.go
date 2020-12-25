6319437877667968cbaa1d651e7b3597fefa6cd6
Alexandrmrn <hardesthackerslogin@gmail.com>
2020-12-25 13:37:26 +0300

    T-1624

--- DBWorker/tests.go
+++ DBWorker/tests.go
@@ -32,3 +32,36 @@ $3
 	_, err := db.Exec(context.Background(), sql, testName, testDesc, usId)
 	return err
 }
+
+
+
+// Возвращает структуру всех тестов доступных автору по его id
+func GetTestsById(authorId in.UsId) ([]in.Test, error) {
+	sql := `
+select tests.id, tests.name, tests.desc, login.login, AVG(test_rate.rate) 
+from tests join test_author on tests.id = test_author.test_id join login on test_author.us_id = login.id left join test_rate on tests.id = test_rate.tes_id 
+where login.id = $1
+group by tests.id, tests.name, tests.desc, login.login 
+order by id
+`
+	tsArr := make([]in.Test, 0)
+	res, err := db.Query(context.Background(), sql, authorId)
+	if err != nil {
+		return nil, err
+	}
+	for res.Next() {
+		var floatPoint *float32
+		ts := in.Test{}
+		err := res.Scan(&ts.TestId, &ts.TestName, &ts.TestDesc, &ts.AuthorName, &floatPoint)
+		if err != nil {
+			return nil, err
+		}
+		if floatPoint == nil {
+			ts.TestRate = 0
+		} else {
+			ts.TestRate = in.TestRate(*floatPoint)
+		}
+		tsArr = append(tsArr, ts)
+	}
+	return tsArr, nil
+}
