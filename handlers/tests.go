c6ec81abf8f858e76285e6e5831f7c144b1bbc36
aicsoonw <47589137+aicsoonw@users.noreply.github.com>
2020-12-25 11:41:20 +0300

    [T-1626] create

--- handlers/tests.go
+++ handlers/tests.go
@@ -52,3 +52,25 @@ func editTest(w http.ResponseWriter, req *http.Request, sessUs *in.SessUs, testI
 	}
 	return err
 }
+
+// Обрабатывает страницу create(Создать Тест)
+func Create(w http.ResponseWriter, req *http.Request, sessUs *in.SessUs) error {
+	_ = req // Переменная необходима для совместительства с типом HandlerIdFunc
+	data := in.DataCreateTest{}
+	var err error
+	if data.Header, err = renderHeader(sessUs.UsId); err != nil {
+		err = errors.New("Ошибка обработки шаблона шапки create " + err.Error())
+		return err
+	}
+	data.Tests, err = db.GetTestsById(sessUs.UsId)
+	if err != nil {
+		err = errors.New("Ошибка получения тестов create " + err.Error())
+		return err
+	}
+	err = renderTemplate(w, in.CreateTestPage, data)
+	if err != nil {
+		err = errors.New("Ошибка обработки шаблона create " + err.Error())
+		return err
+	}
+	return nil
+}
